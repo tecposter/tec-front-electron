f72fa5600d18d9995dbe1697ef0842406e28cae9
Anthony Zhan <anthony_zhan@epam.com>
2020-02-28 19:00:17 +0800

    PostCtn style

--- src/js/component/PostCtn.js
+++ src/js/component/PostCtn.js
@@ -4,8 +4,28 @@ export default class PostCtn extends MultiCol {
   constructor() {
     super(1, 1, 'post-ctn');
 
-    // this.side = this.getCol(0);
-    // this.main = this.getCol(1);
+    this.side = this.getCol(0);
+    this.main = this.getCol(1);
+
+    this.showSideBtn = this.main.oneElem('.show-side');
+    this.hideSideBtn = this.main.oneElem('.hide-side');
+
+    this.showSideBtn.on('click', () => this.showSide());
+    this.hideSideBtn.on('click', () => this.hideSide());
+
+    this.hideSide();
+  }
+
+  hideSide() {
+    this.side.hide();
+    this.hideSideBtn.hide();
+    this.showSideBtn.show();
+  }
+
+  showSide() {
+    this.side.show();
+    this.showSideBtn.hide();
+    this.hideSideBtn.show();
   }
 
   initCols() {
@@ -19,7 +39,13 @@ export default class PostCtn extends MultiCol {
 
     this.getCol(1).html`
     <div class="head">
-      main head
+      <a class="show-side toggle">
+        Show Side
+        &gt;&gt;
+      </a>
+      <a class="hide-side toggle">
+        &lt;&lt;
+      </a>
     </div>
     <div class="body">
     </div>
